89c0e0ce5bfb8837061dad5b9f482b21f375e63d
Baharuddin Aziz <baha.bdg@gmail.com>
2016-09-03 03:48:18 +0700

    Init transpos-matriks.pas

---
+++ bab-1-pengenalan/1a-masukan-dan-keluaran/transpos-matriks.pas
@@ -0,0 +1,28 @@
+program transpos_matriks;
+
+var
+	a : integer;
+	b : integer;
+	c : integer;
+	d : integer;
+	e : integer;
+	f : integer;
+	g : integer;
+	h : integer;
+	i : integer;
+	
+begin
+	read(a);
+	read(b);
+	read(c);
+	read(d);
+	read(e);
+	read(f);
+	read(g);
+	read(h);
+	read(i);
+	
+	writeln(a, ' ', d, ' ', g);
+	writeln(b, ' ', e, ' ', h);
+	writeln(c, ' ', f, ' ', i);
+end.

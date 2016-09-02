639817d8c91403cca69a64668f23435c6b3fb9df
Baharuddin Aziz <baha.bdg@gmail.com>
2016-09-03 03:47:07 +0700

    Init a-tambah-b.pas

---
+++ bab-1-pengenalan/1a-masukan-dan-keluaran/a-tambah-b.pas
@@ -0,0 +1,13 @@
+program a_tambah_b;
+
+var
+	input_a : integer;
+	input_b : integer;
+	output	: integer;
+
+begin
+	read(input_a);
+	read(input_b);
+	output := input_a + input_b;
+	writeln(output);
+end.

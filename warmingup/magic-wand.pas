4894b0bac380c72b724874c087e9722c5f4b8896
Baharuddin Aziz <baha.bdg@gmail.com>
2016-10-09 22:17:51 +0700

    Update magic-wand.pas 1. Replace tab with spaces.

--- warmingup/magic-wand.pas
+++ warmingup/magic-wand.pas
@@ -9,27 +9,27 @@ program magic_wand;
  
 { DEKLARASI VARIABEL }
 var
-	// input
-	Ni : longint;	// jumlah tongkat pada loker i
-	
-	// output
-	M : longint;	// jumlah tongkat sihir milik Wembo
-	
+    // input
+    Ni : longint;   // jumlah tongkat pada loker i
+    
+    // output
+    M : longint;    // jumlah tongkat sihir milik Wembo
+    
 begin
-	{ INISIALISASI VARIABEL }
-	// inisialisasi
-	M := 0;
-	
-	// membaca masukan user
-	read(Ni);
-	while (Ni >= 1) and (Ni <= 1000000) do
-	begin
-		M := M + Ni;
-		read(Ni);
-	end;
+    { INISIALISASI VARIABEL }
+    // inisialisasi
+    M := 0;
+    
+    // membaca masukan user
+    read(Ni);
+    while (Ni >= 1) and (Ni <= 1000000) do
+    begin
+        M := M + Ni;
+        read(Ni);
+    end;
 
-	{ ALGORITMA }
-	// menuliskan jumlah tongkat sihir
-	M := M mod 1000000007;
-	writeln(M);
+    { ALGORITMA }
+    // menuliskan jumlah tongkat sihir
+    M := M mod 1000000007;
+    writeln(M);
 end.
